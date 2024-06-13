0e67b50354c4188b77e06d73c8f4999702cdab01
MacJ13 <jacdim02@gmail.com>
2024-06-13 21:04:25 +0200

    Hash user password with bcrypt package
    Create bcrypt config file

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,7 @@
+const { SALT_ROUNDS } = require("../configs/bcrypt.config");
 const User = require("../models/user.model");
+
+const bcrypt = require("bcrypt");
 
 // exports.login_user = async(req, res) => {
 //     const
@@ -12,16 +15,33 @@ exports.signup_user = async (req, res) => {
 
   if (existUser) return res.status(400).json({ message: "user exists!" });
 
-  const newUser = new User({
-    nickname: req.body.nickname,
-    email: req.body.email,
-    password: req.body.password,
-    favorites: [],
+  bcrypt.hash(req.body.password, SALT_ROUNDS, async function (err, hash) {
+    if (err) return res.status(400).json({ err });
+
+    const newUser = new User({
+      nickname: req.body.nickname,
+      email: req.body.email,
+      password: hash,
+      favorites: [],
+    });
+
+    await newUser.save();
+
+    return res.status(200).json({ message: "user sign in" });
   });
 
-  await newUser.save();
+  // bcrypt.hash(req.body.password, SALT_ROUNDS).then(async function (result) {
+  //   const newUser = new User({
+  //     nickname: req.body.nickname,
+  //     email: req.body.email,
+  //     password: req.body.password,
+  //     favorites: [],
+  //   });
 
-  return res.status(200).json({ message: "user sign in" });
+  //   await newUser.save();
+
+  //   return res.status(200).json({ message: "user sign in" });
+  // });
 };
 
 exports.login_user = async (req, res) => {
@@ -29,8 +49,10 @@ exports.login_user = async (req, res) => {
 
   if (!existUser)
     return res.status(400).json({ message: "user does not exist" });
-  else if (existUser.password !== req.body.password)
-    return res.status(400).json({ message: "incorrect password" });
+
+  const match = await bcrypt.compare(req.body.password, existUser.password);
+
+  if (!match) return res.status(400).json({ message: "incorrect password" });
 
   return res
     .status(200)
