70ba5149e0bdb66745cda270ed6a19ff8fa40985
MacJ13 <jacdim02@gmail.com>
2024-07-24 22:28:03 +0200

    Check authorization before user, post and comment routes

--- routes/api/api.route.js
+++ routes/api/api.route.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const indexRouter = require("./index.route");
 const userRouter = require("./user.route");
-
 const postRouter = require("./post.route");
 const commentRouter = require("./comment.route");
 
-router.use("/", indexRouter);
+const { checkAuth } = require("../../services/passport.jwt.service");
+
+router.use(checkAuth);
 router.use("/user", userRouter);
 router.use("/post", postRouter);
 router.use("/comment", commentRouter);
