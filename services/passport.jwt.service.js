6a6403d06f9ad4a1c2a303055304803b3e4ac31b
MacJ13 <jacdim02@gmail.com>
2024-07-24 22:12:47 +0200

    Handle refresh token

--- services/passport.jwt.service.js
+++ services/passport.jwt.service.js
@@ -70,7 +70,103 @@ const checkAuth = (req, res, next) => {
   )(req, res, next);
 };
 
+const handleRefreshToken = async (req, res) => {
+  // get cookies from request cookies;
+  const cookies = req.cookies;
+
+  // check if coookies with jwt exists
+  if (!cookies?.jwt)
+    return res.status(401).json({ msg: "Unauthorized message!" });
+
+  // assign cokkies jwt as refresh token
+  const refreshToken = cookies.jwt;
+
+  // find user from mongoDB with refreshToken
+  const foundUser = await User.findOne({
+    refreshToken: { $in: [refreshToken] },
+  });
+
+  // check if user exists
+  if (!foundUser) {
+    jwt.verify(
+      refreshToken,
+      process.env.JWT_REFRESH_KEY,
+      async (err, decoded) => {
+        if (err) return res.status(403).json({ msg: "Forbidden user!" });
+
+        const { id } = decoded;
+        // console.log({ decoded });
+        // const hackedUser = await User.findOne({
+        //   username: decoded.nickname,
+        // }).exec();
+        const hackedUser = await User.findById(id).exec();
+        // console.log({ hackedUser });
+        if (hackedUser) {
+          hackedUser.refreshToken = [];
+
+          await hackedUser.save();
+        }
+      }
+    );
+
+    return res.status(403).json({ msg: "Forbidden user!" });
+  }
+
+  // filter fresh array token to remove current fresh token
+  const newRefreshTokenArray = foundUser.refreshToken.filter(
+    (rt) => rt !== refreshToken
+  );
+
+  // evaluate jwt
+  jwt.verify(
+    refreshToken,
+    process.env.JWT_REFRESH_KEY,
+    async (err, decoded) => {
+      if (err) {
+        foundUser.refreshToken = [...newRefreshTokenArray];
+        // const result =
+        await foundUser.save();
+      }
+
+      if (err || foundUser.nickname !== decoded.nickname) {
+        return res.status(403).json({ msg: "Forbidden user!" });
+      }
+
+      // Referes token was still valid
+      const userData = {
+        email: foundUser.email,
+        nickname: foundUser.nickname,
+        id: foundUser._id,
+      };
+
+      // Create new access token and refresh toekn
+      const accessToken = jwt.sign(userData, process.env.JWT_SECRET_KEY, {
+        expiresIn: "300s",
+      });
+
+      const newRefreshToken = jwt.sign(userData, process.env.JWT_REFRESH_KEY, {
+        expiresIn: "1d",
+      });
+
+      // saving RefreshToken with current user
+      foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+
+      await foundUser.save();
+
+      res.cookie("jwt", newRefreshToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+
+      res.json({ accessToken });
+    }
+  );
+};
+
+// move and rename handleLogout to user_logout
+
 module.exports = {
   jwt_strategy,
   checkAuth,
+  handleRefreshToken,
 };
