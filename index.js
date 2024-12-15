51157edabab4a4bcc8430952f431db841ac04407
MacJ13 <jacdim02@gmail.com>
2024-12-15 16:57:20 +0100

    Add cors options

--- index.js
+++ index.js
@@ -22,10 +22,16 @@ connectDB();
 
 passport.use(jwt_strategy);
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // app.use("/", indexRouter);
 app.use("/api", apiRouter);
